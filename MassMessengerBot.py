566862f64923a2f256dc074aaf704970ae27d82c
Teo Brown <teobrown@Teos-MBP.Starry-inc.net>
2022-08-11 06:27:13 -0400

    First Failure of Mass Messenger

--- MassMessengerBot.py
+++ MassMessengerBot.py
@@ -9,14 +9,14 @@ driver = webdriver.Chrome()
 url = 'https://www.instagram.com/'
 driver.get(url)
 
-username = 'teobear322'
-password = 'Chess101'
+my_username = 'teobear322'
+my_password = 'Chess101'
 
 wait_time = 10
 
-person = 'miriam.oke'
+targets = ['abbyvee_', 'iwillstealyouroreos', 'jordanxalexis2']
 
-text = 'I just texted u off a bot niggggggaaaaaa'
+text = "HAHAHAHAHAHAHAH IT REALLY WORKS. You got texted off of a bot made by the glorius Mateo Brown. This is only the first step in my plan. You have been warned by the Loud House. In one month you will learn the true extent of my power"
 
 def sign_in(username, password):
     driver.implicitly_wait(wait_time)
@@ -37,9 +37,9 @@ def page_contents():
     #Message Button
     driver.find_element("xpath", '/html/body/div[1]/div/div/div/div[1]/div/div/div/div[1]/div[1]/section/nav/div[2]/div/div/div[3]/div/div[2]/a').click()
     driver.implicitly_wait(wait_time)
-    driver.find_element("xpath", '/html/body/div[1]/div/div/div/div[1]/div/div/div/div[1]/div[1]/div/section/div/div[2]/div/div/div[1]/div[1]/div/div[3]/button/div').click()
-
-def message_page(user):
+    
+def message(user):
+    driver.implicitly_wait(wait_time)
     #type name into messenger
     driver.find_element("xpath", '/html/body/div[1]/div/div/div/div[2]/div/div/div[1]/div/div[2]/div/div/div/div/div/div/div[2]/div[1]/div/div[2]/input').send_keys(user)
     #Account Clicker
@@ -51,12 +51,14 @@ def message_page(user):
 def send_message(text):
     #Text
     driver.find_element("xpath", '/html/body/div[1]/div/div/div/div[1]/div/div/div/div[1]/div[1]/div/section/div/div[2]/div/div/div[2]/div[2]/div/div[2]/div/div/div[2]/textarea').send_keys(text)
-
     #Send
     driver.find_element("xpath", '/html/body/div[1]/div/div/div/div[1]/div/div/div/div[1]/div[1]/div/section/div/div[2]/div/div/div[2]/div[2]/div/div[2]/div/div/div[3]/button').click()
     print("Message successful")
 
-sign_in('teobear322','Chess101')
+sign_in(my_username, my_password)
 page_contents()
-message_page(person)
-send_message("HAHAHAHAHAHAHAH IT REALLY WORKS. You got texted off of a bot made by the glorius Mateo Brown. This is only the first step in my plan. You have been warned by the Loud House. In one month you will learn the true extent of my power")
+for target in targets:
+    #Write Button
+    driver.find_element("xpath", '/html/body/div[1]/div/div/div/div[1]/div/div/div/div[1]/div[1]/div/section/div/div[2]/div/div/div[1]/div[1]/div/div[3]/button/div').click()
+    message(target)
+    send_message(text)
