8cbee4752127170ca3bb44fd8a966e5d447af198
Teo Brown <teobrown@Teos-MBP.Starry-inc.net>
2022-08-11 07:27:16 -0400

    Finished Mass Messenger

--- MassMessengerBot.py
+++ MassMessengerBot.py
@@ -1,8 +1,10 @@
 from multiprocessing.connection import wait
+from operator import truediv
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import os
 import time
+import random
 
 driver = webdriver.Chrome()
 
@@ -14,9 +16,11 @@ my_password = 'Chess101'
 
 wait_time = 10
 
-targets = ['lor_bernie', 'jordanxalexis2']
+targets = ['lor_bernie', 'jordanxalexis2', 'itsday_g', 'elijahrock','miriam.oke']
 
 text = "HAHAHAHAHAHAHAH IT REALLY WORKS. You got texted off of a bot made by the glorius Mateo Brown. This is only the first step in my plan. You have been warned by the Loud House. In one month you will learn the true extent of my power"
+
+time_between_text = 2
 
 def sign_in(username, password):
     driver.implicitly_wait(wait_time)
@@ -41,7 +45,7 @@ def page_contents():
 def message(user):
     driver.implicitly_wait(wait_time)
     #type name into messenger
-    driver.find_element("xpath", '/html/body/div[1]/div/div/div/div[2]/div/div/div[1]/div/div[2]/div/div/div/div/div/div/div[2]/div[1]/div/div[2]/input').send_keys(user)
+    textarea = driver.find_element("xpath", '/html/body/div[1]/div/div/div/div[2]/div/div/div[1]/div/div[2]/div/div/div/div/div/div/div[2]/div[1]/div/div[2]/input').send_keys(user)
     driver.implicitly_wait(wait_time)
     #Account Clicker
     driver.find_element("xpath", '/html/body/div[1]/div/div/div/div[2]/div/div/div[1]/div/div[2]/div/div/div/div/div/div/div[2]/div[2]/div[1]/div/div[3]/button/div').click()
@@ -54,12 +58,14 @@ def send_message(text):
         driver.implicitly_wait(wait_time)
         #Text
         driver.find_element("xpath", '/html/body/div[1]/div/div/div/div[1]/div/div/div/div[1]/div[1]/div/section/div/div[2]/div/div/div[2]/div[2]/div/div[2]/div/div/div[2]/textarea').send_keys(text)
+        driver.implicitly_wait(wait_time)
         #Send
         driver.find_element("xpath", '/html/body/div[1]/div/div/div/div[1]/div/div/div/div[1]/div[1]/div/section/div/div[2]/div/div/div[2]/div[2]/div/div[2]/div/div/div[3]/button').click()
-        print("Message successful")
+        return True
     
     except Exception as err:
         print(err)
+        return False
 
 sign_in(my_username, my_password)
 page_contents()
@@ -67,4 +73,8 @@ for target in targets:
     #Write Button
     driver.find_element("xpath", '/html/body/div[1]/div/div/div/div[1]/div/div/div/div[1]/div[1]/div/section/div/div[2]/div/div/div[1]/div[1]/div/div[3]/button/div').click()
     message(target)
+    if message:
+        print(f"Message to \"{target}\" was succesful")
+    time.sleep(random.randint(2,3))
     send_message(text)
+    time.sleep(time_between_text)
